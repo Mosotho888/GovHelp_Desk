498f7593daa8cb14719a3b28f817133aeba6cb93
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2024-08-23 12:57:15 +0200

    initial commit

---
+++ springjwtauth/src/main/java/com/loggingsystem/springjwtauth/SpringjwtauthApplication.java
@@ -0,0 +1,13 @@
+package com.loggingsystem.springjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtauthApplication.class, args);
+	}
+
+}
