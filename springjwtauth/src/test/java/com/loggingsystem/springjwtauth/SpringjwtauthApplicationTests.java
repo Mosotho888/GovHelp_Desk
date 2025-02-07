3281eef664c0d9a6652e0067474d5c7dcadd5ccf
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2025-02-08 01:02:30 +0200

    refactor the code, added email notifications and documentation

--- springjwtauth/src/test/java/com/loggingsystem/springjwtauth/SpringjwtauthApplicationTests.java
+++ springjwtauth/src/test/java/com/loggingsystem/springjwtauth/SpringjwtauthApplicationTests.java
@@ -1,9 +1,12 @@
 package com.loggingsystem.springjwtauth;
 
+import com.loggingsystem.springjwtauth.config.messaging.MailConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(MailConfig.class)
 class SpringjwtauthApplicationTests {
 
 
