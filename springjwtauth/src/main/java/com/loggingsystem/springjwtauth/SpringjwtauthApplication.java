e835a4aa436d7eac9e87d80483c934952982f884
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2025-02-08 20:34:01 +0200

    refactor the code, added email notifications and documentation

--- springjwtauth/src/main/java/com/loggingsystem/springjwtauth/SpringjwtauthApplication.java
+++ springjwtauth/src/main/java/com/loggingsystem/springjwtauth/SpringjwtauthApplication.java
@@ -8,11 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringjwtauthApplication {
 
 	public static void main(String[] args) {
-		// Load environment variables from .env file
-		Dotenv dotenv = Dotenv.load();
 
-		// Set variables to System properties (Spring Boot reads from here)
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(SpringjwtauthApplication.class, args);
 	}
 
