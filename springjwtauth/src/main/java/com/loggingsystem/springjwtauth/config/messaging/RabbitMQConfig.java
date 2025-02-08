158d3de60623de37826de15e6e565eb2ab10983f
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2025-02-08 14:13:30 +0200

    refactor the code, added email notifications and documentation

--- springjwtauth/src/main/java/com/loggingsystem/springjwtauth/config/messaging/RabbitMQConfig.java
+++ springjwtauth/src/main/java/com/loggingsystem/springjwtauth/config/messaging/RabbitMQConfig.java
@@ -6,10 +6,12 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(RabbitMQProperties.class)
 public class RabbitMQConfig {
     private final RabbitMQProperties properties;
 
