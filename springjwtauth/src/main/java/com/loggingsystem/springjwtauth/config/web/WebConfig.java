e9c097d58c9461495e5adf926b009ed2a9ab7ac8
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2025-02-11 15:44:45 +0200

    modified cors config

--- springjwtauth/src/main/java/com/loggingsystem/springjwtauth/config/web/WebConfig.java
+++ springjwtauth/src/main/java/com/loggingsystem/springjwtauth/config/web/WebConfig.java
@@ -1,20 +1,25 @@
 package com.loggingsystem.springjwtauth.config.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableMethodSecurity
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")  // Adjust the path to match your API endpoints
-                .allowedOrigins("http://localhost:3000", "https://govhelpdesk-production.up.railway.app/")  // Allow frontend origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
-                .allowedHeaders("*")  // Allow all headers
-                .allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")  // Adjust the path to match your API endpoints
+                        .allowedOrigins("http://localhost:3000", "https://govhelpdesk-production.up.railway.app/")  // Allow frontend origin
+                        .allowedMethods("*")  // Allowed HTTP methods
+                        .allowedHeaders("*")  // Allow all headers
+                        .allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
+            }
+        };
     }
 }
