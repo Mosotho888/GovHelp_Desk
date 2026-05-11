b2b347a6d7f6ec96b00772fb6ff7f4a9ef43e61e
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 10:16:32 +0200

    feature: implemented Agent dto Response

---
+++ helpdesk/src/main/java/za/gov/helpdesk/agent/dto/AgentResponse.java
@@ -0,0 +1,16 @@
+package za.gov.helpdesk.agent.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import za.gov.helpdesk.agent.model.Agent;
+import za.gov.helpdesk.users.dto.UserResponse;
+
+@Data
+@Builder
+public class AgentResponse {
+
+    private Long id;
+    private UserResponse user;
+    private String department;
+    private Agent.Availability availability;
+}
