69c54fd63ac24797290991be12c744a1e736b6ad
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 10:03:58 +0200

    feature: implemented Agent dto Request

---
+++ helpdesk/src/main/java/za/gov/helpdesk/agent/dto/CreateAgentRequest.java
@@ -0,0 +1,15 @@
+package za.gov.helpdesk.agent.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NonNull;
+import za.gov.helpdesk.agent.model.Agent;
+
+@Data
+public class CreateAgentRequest {
+
+    @NotNull(message = "userId is required")
+    private String userId;
+    private String department;
+    private Agent.Availability availability = Agent.Availability.OFFLINE;
+}
