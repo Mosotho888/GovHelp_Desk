65398a1f61ba79803278e0aa987bda5aeb01092b
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 10:05:44 +0200

    feature: implemented Agent update dto Request

---
+++ helpdesk/src/main/java/za/gov/helpdesk/agent/dto/UpdateAgentRequest.java
@@ -0,0 +1,11 @@
+package za.gov.helpdesk.agent.dto;
+
+import lombok.Data;
+import za.gov.helpdesk.agent.model.Agent;
+
+@Data
+public class UpdateAgentRequest {
+
+    private Agent.Availability availability;
+    private String department;
+}
