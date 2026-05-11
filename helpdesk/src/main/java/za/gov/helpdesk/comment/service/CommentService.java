35c06db380c56ea9b2236d5186f01c3d1f55cf6b
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 21:23:49 +0200

    feature: implemented methods signatures

---
+++ helpdesk/src/main/java/za/gov/helpdesk/comment/service/CommentService.java
@@ -0,0 +1,18 @@
+package za.gov.helpdesk.comment.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import za.gov.helpdesk.comment.dto.CommentResponse;
+import za.gov.helpdesk.comment.dto.CreateCommentRequest;
+
+import java.util.List;
+
+public interface CommentService {
+
+    CommentResponse addComment(Long ticketId, CreateCommentRequest request);
+    CommentResponse addReply(Long parentCommentId, CreateCommentRequest request);
+    Page<CommentResponse> getComments(Long ticketId, Pageable pageable);
+    List<CommentResponse> getReplies(Long commentId);
+    CommentResponse updateComment(Long commentId, CreateCommentRequest request);
+    void deleteComment(Long commentId);
+}
--- helpdesk/src/main/java/za/gov/helpdesk/comment/service/TicketCommentService.java
+++
@@ -1,4 +0,0 @@
-package za.gov.helpdesk.comment.service;
-
-public interface TicketCommentService {
-}
