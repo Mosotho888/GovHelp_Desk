848206b5b19ad87ddb3d76bc601ff14370fe9e39
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 22:30:57 +0200

    feature: implemented AttachmentRepository

---
+++ helpdesk/src/main/java/za/gov/helpdesk/attachment/repository/AttachmentRepository.java
@@ -0,0 +1,15 @@
+package za.gov.helpdesk.attachment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.gov.helpdesk.attachment.model.Attachment;
+
+import java.util.List;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+    List<Attachment> findByTicketId(Long ticketId);
+
+    long countByTicketId(Long ticketId);
+}
