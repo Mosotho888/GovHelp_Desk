b2818d3a5da70e40d0d14ea39de2f5d7387fb60e
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 21:07:33 +0200

    feature: implemented UpdateCommentRequest dto

---
+++ helpdesk/src/main/java/za/gov/helpdesk/comment/dto/UpdateCommentRequest.java
@@ -0,0 +1,13 @@
+package za.gov.helpdesk.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateCommentRequest {
+
+    @NotBlank(message = "Comment body is required")
+    @Size(max = 10000, message = "Comment must not exceed 10,000 characters")
+    private String body;
+}
