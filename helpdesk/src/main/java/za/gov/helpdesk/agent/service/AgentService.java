25644c25a0b3876481a7e9f73212a38b77cd5dcf
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-11 10:28:50 +0200

    feature: implemented Agent service interface

---
+++ helpdesk/src/main/java/za/gov/helpdesk/agent/service/AgentService.java
@@ -0,0 +1,18 @@
+package za.gov.helpdesk.agent.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import za.gov.helpdesk.agent.dto.AgentResponse;
+import za.gov.helpdesk.agent.dto.AgentStatsResponse;
+import za.gov.helpdesk.agent.dto.CreateAgentRequest;
+import za.gov.helpdesk.agent.dto.UpdateAgentRequest;
+
+public interface AgentService {
+
+    AgentResponse createAgent(CreateAgentRequest request);
+    AgentResponse getAgentById(Long id);
+    Page<AgentResponse> getAllAgents(Pageable pageable);
+    AgentStatsResponse getAgentStats(Long id);
+    AgentResponse updateAgent(UpdateAgentRequest request);
+
+}
