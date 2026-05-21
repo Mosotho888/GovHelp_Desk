f5f39fe271288df98b229022bf4cb479f34abb72
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-21 15:10:04 +0200

    feature: implemented rabbitmq infrastructure

--- helpdesk/src/main/java/za/gov/helpdesk/config/messaging/RabbitMQConfig.java
+++ helpdesk/src/main/java/za/gov/helpdesk/config/messaging/RabbitMQConfig.java
@@ -1,91 +1,89 @@
 package za.gov.helpdesk.config.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
 public class RabbitMQConfig {
-    private final RabbitMQProperties properties;
 
-    public RabbitMQConfig(RabbitMQProperties properties) {
-        this.properties = properties;
+    @Value("${app.rabbitmq.concurrent-consumers}")
+    private int concurrentConsumers;
+
+    @Value("${app.rabbitmq.max-concurrent-consumers}")
+    private int maxConcurrentConsumers;
+
+    @Bean
+    public TopicExchange helpdeskExchange() {
+        return new TopicExchange(RabbitMQConstants.EXCHANGE);
+    }
+
+    @Bean
+    public Queue auditQueue() {
+        return QueueBuilder.durable(RabbitMQConstants.AUDIT_QUEUE).build();
+    }
+
+    @Bean
+    public Queue emailQueue() {
+        return QueueBuilder.durable(RabbitMQConstants.EMAIL_QUEUE).build();
+    }
+
+    @Bean
+    public Binding bindAuditQueue(Queue auditQueue, TopicExchange helpdeskExchange) {
+        return BindingBuilder.bind(auditQueue).to(helpdeskExchange).with(RabbitMQConstants.AUDIT_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindEmailQueue(Queue emailQueue, TopicExchange helpdeskExchange) {
+        return BindingBuilder.bind(emailQueue).to(helpdeskExchange).with(RabbitMQConstants.EMAIL_ROUTING_KEY);
     }
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
-        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(objectMapper);
+
         DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
-        typeMapper.setTrustedPackages("com.loggingsystem.springjwtauth.dto"); // Crucial!
+        typeMapper.setTrustedPackages("za.gov.helpdesk"); // Crucial!
         converter.setJavaTypeMapper(typeMapper);
+
         return converter;
     }
+
     @Bean
-    public Queue ticketAssignmentQueue() {
-        log.info("TAQ: {}", properties.getTechnicianAssignmentQueue());
-        return new Queue(properties.getTechnicianAssignmentQueue(), false);
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+
+        return rabbitTemplate;
     }
 
     @Bean
-    public Queue ticketStatusChangeQueue() {
-        return new Queue(properties.getTicketStatusChangeQueue(), false);
-    }
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter jsonMessageConverter) {
 
-    @Bean
-    public Queue ticketCommentQueue() {
-        return new Queue(properties.getTicketCommentQueue(), false);
-    }
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter);
 
-    @Bean
-    public Queue ticketCreationQueue() {
-        return new Queue(properties.getTicketCreationQueue(), false);
-    }
+        factory.setConcurrentConsumers(concurrentConsumers);
+        factory.setMaxConcurrentConsumers(maxConcurrentConsumers);
 
-    @Bean
-    public DirectExchange ticketAssignmentExchange() {
-        return new DirectExchange(properties.getTechnicianAssignmentExchange());
-    }
-
-    @Bean
-    public DirectExchange ticketStatusChangeExchange() {
-        return new DirectExchange(properties.getTicketStatusChangeExchange());
-    }
-
-    @Bean
-    public DirectExchange ticketCommentExchange() {
-        return new DirectExchange(properties.getTicketCommentExchange());
-    }
-
-    @Bean
-    public DirectExchange ticketCreationExchange() {
-        log.info("config TCEx: {}", properties.getTicketCreationExchange());
-        return new DirectExchange(properties.getTicketCreationExchange());
-    }
-
-    @Bean
-    public Binding ticketAssignmentBinding(Queue ticketAssignmentQueue, DirectExchange ticketAssignmentExchange) {
-        return BindingBuilder.bind(ticketAssignmentQueue).to(ticketAssignmentExchange).with(properties.getTechnicianAssignedRoutingKey());
-    }
-
-    @Bean
-    public Binding ticketStatusChangeBinding(Queue ticketStatusChangeQueue, DirectExchange ticketStatusChangeExchange) {
-        return BindingBuilder.bind(ticketStatusChangeQueue).to(ticketStatusChangeExchange).with(properties.getTicketStatusChangedRoutingKey());
-    }
-
-    @Bean
-    public Binding ticketCommentBinding(Queue ticketCommentQueue, DirectExchange ticketCommentExchange) {
-        return BindingBuilder.bind(ticketCommentQueue).to(ticketCommentExchange).with(properties.getTicketCommentAddedRoutingKey());
-    }
-
-    @Bean
-    public Binding ticketCreationBiding(Queue ticketCreationQueue, DirectExchange ticketCreationExchange) {
-        return BindingBuilder.bind(ticketCreationQueue).to(ticketCreationExchange).with(properties.getTicketCreatedRoutingKey());
+        return factory;
     }
 }
---
+++ helpdesk/src/main/java/za/gov/helpdesk/config/messaging/RabbitMQConstants.java
@@ -0,0 +1,20 @@
+package za.gov.helpdesk.config.messaging;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public final class RabbitMQConstants {
+
+    public static final String EXCHANGE = "helpdesk.exchange";
+
+    public static final String AUDIT_QUEUE = "audit.queue";
+    public static final String EMAIL_QUEUE = "email.queue";
+
+    public static final String AUDIT_ROUTING_KEY = "audit.#";
+    public static final String EMAIL_ROUTING_KEY = "email.#";
+}
