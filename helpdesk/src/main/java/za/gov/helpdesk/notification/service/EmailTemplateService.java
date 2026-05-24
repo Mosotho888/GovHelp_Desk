f71419678ad67439b3aa8f5903dbb21e821920ed
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-05-24 12:15:38 +0200

    feature: added EmailTemplateService interface abstract methods

---
+++ helpdesk/src/main/java/za/gov/helpdesk/notification/service/EmailTemplateService.java
@@ -0,0 +1,15 @@
+package za.gov.helpdesk.notification.service;
+
+import za.gov.helpdesk.notification.dto.EmailNotificationMessage;
+
+public interface EmailTemplateService {
+
+    String ticketCreatedCustomer(EmailNotificationMessage msg);
+    String ticketCreatedAgent(EmailNotificationMessage msg);
+    String ticketAssignedCustomer(EmailNotificationMessage msg);
+    String ticketAssignedAgent(EmailNotificationMessage msg);
+    String statusChangedCustomer(EmailNotificationMessage msg);
+    String commentAddedCustomer(EmailNotificationMessage msg);
+    String commentAddedAgent(EmailNotificationMessage msg);
+    String ticketClosedCustomer(EmailNotificationMessage msg);
+}
---
+++ helpdesk/src/main/java/za/gov/helpdesk/notification/service/impl/EmailServiceImpl.java
@@ -0,0 +1,7 @@
+package za.gov.helpdesk.notification.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl {
+}
