34531e240273aaa10a674a3bc572be9c0653c08a
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-06-05 11:06:05 +0200

    refactor(Notification): deleted, moved from method html and css call to thymeleaf

--- helpdesk/src/main/java/za/gov/helpdesk/notification/service/auth/AuthEmailTemplateService.java
+++
@@ -1,9 +0,0 @@
-package za.gov.helpdesk.notification.service.auth;
-
-import za.gov.helpdesk.notification.dto.PasswordResetEmailNotificationMessage;
-import za.gov.helpdesk.notification.dto.TicketEmailNotificationMessage;
-
-public interface AuthEmailTemplateService {
-
-    String passwordResetOtp(PasswordResetEmailNotificationMessage message);
-}
