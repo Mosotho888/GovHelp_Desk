f6dbc2c817c98626c563c98f75ee52ad3d9fcf2d
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-06-28 17:23:50 +0200

    Add Server import to OpenApiConfig
    Added import for Server model to OpenApiConfig.

--- helpdesk/src/main/java/za/gov/helpdesk/config/security/OpenApiConfig.java
+++ helpdesk/src/main/java/za/gov/helpdesk/config/security/OpenApiConfig.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
 
 /**
  * Configuration component responsible for exposing and customising the OpenAPI v3 specifications.
