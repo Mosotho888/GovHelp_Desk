3c25132f5a9880118ddd470a846efe832ab88420
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-07-31 23:57:00 +0200

    added https://govhelpdesk.sothoman.com to allowed origin

--- helpdesk/src/main/java/za/gov/helpdesk/config/web/CorsConfig.java
+++ helpdesk/src/main/java/za/gov/helpdesk/config/web/CorsConfig.java
@@ -34,7 +34,11 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
+        configuration.setAllowedOrigins(
+                List.of(
+                        "http://localhost:5173",
+                        "http://localhost:8080",
+                        "https://govhelpdesk.sothoman.com"));
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
